7f888ecd029ee93843c630e88996377e9b119f5b
Laurence McLean <lmclean81@gmail.com>
2026-06-03 15:01:22 +0930

    jcs_tool: Update user_external logging

--- jcs_tool/jcs_user_external.cpp
+++ jcs_tool/jcs_user_external.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2024 Arbite Robotics Pty Ltd
 // https://arbite.io
 //
+#include <cstdio>
 #include <ctime>
 #include "jcs_user_external.h"
 #include "task_rt.h"
@@ -20,6 +21,23 @@ long int time_now_ns() {
     return ts.tv_sec * nsec_per_sec + ts.tv_nsec;
 }
 
+// Default implementation: prepend level prefix and print to stdout.
+// Replace this to route JCS output to your logging system.
+void print_output(log_level level, bool prefix, const char* msg) {
+    if (prefix) {
+        const char* pfx = "";
+        switch (level) {
+            case log_level::info:    pfx = "INFO   : "; break;
+            case log_level::debug:   pfx = "DEBUG  : "; break;
+            case log_level::warning: pfx = "WARNING: "; break;
+            case log_level::error:   pfx = "ERROR! : "; break;
+            default:                 pfx = "UNHANDLED!: "; break;
+        }
+        printf("%s%s", pfx, msg);
+    } else {
+        printf("%s", msg);
+    }
+}
 
 } // End namespace external
 } // End namespace jcs
